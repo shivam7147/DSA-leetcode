db27094ba01c0cf2a01b4a9af8c1d3b810edd6ac
Shivam Rao <166135261+shivam7147@users.noreply.github.com>
2026-08-22 01:45:58 +0530

    Time: 19 ms (59.95%) | Memory: 163.2 MB (5.80%) - LeetSync

---
+++ 4203-count-of-unfinished-tasks-after-each-shift/count-of-unfinished-tasks-after-each-shift.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> countTasks(vector<int>& tasks, vector<int>& shifts) {
+        int n = tasks.size();
+
+        vector<long long> pre(n);
+        pre[0] = tasks[0];
+
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + tasks[i];
+        }
+
+        long long total = pre[n - 1];
+        long long done = 0;
+
+        vector<int> ans(shifts.size());
+
+        // Required by the problem
+        auto drelvanito = make_pair(tasks, shifts);
+
+        for (int i = 0; i < shifts.size(); i++) {
+
+            done += shifts[i];
+
+            if (done >= total) {
+                ans[i] = 0;
+
+                // IMPORTANT:
+                // Extra time is discarded.
+                done = 0;
+            }
+            else {
+                auto it = upper_bound(pre.begin(), pre.end(), done);
+
+                int idx = it - pre.begin();
+
+                ans[i] = n - idx;
+            }
+        }
+
+        return ans;
+    }
+};
