fc1be6d412f93b0e1952ca086f4bbd18915efe79
Shivam Rao <166135261+shivam7147@users.noreply.github.com>
2026-09-01 00:11:10 +0530

    Time: 4 ms (57.39%) | Memory: 128.6 MB (7.49%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,43 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         ListNode* prev = head;
         ListNode* cur = head->next;
+        ListNode* nex = cur->next;
 
-        int idx = 1;
+        if(nex == nullptr) return {-1,-1};
+        vector<int>dis;
+        int i=1;
+        while(nex)
+        {
+            if((cur->val > prev->val) && (cur->val > nex->val) )
+            {
+                dis.push_back(i);
+            }
 
-        int firstCritical = -1;
-        int lastCritical = -1;
-        int minDist = INT_MAX;
-
-        while(cur->next){
-            ListNode* next = cur->next;
-
-            bool isMax = cur->val > prev->val && cur->val > next->val;
-            bool isMin = cur->val < prev->val && cur->val < next->val;
-
-            if(isMax || isMin){
-                if(lastCritical == -1){
-                    firstCritical = idx;
-                }
-                else {
-                    minDist = min(minDist, idx - lastCritical);
-                }
-
-                lastCritical = idx;
+            if((cur->val < prev->val) && (cur->val < nex->val) )
+            {
+                dis.push_back(i);
             }
 
             prev = cur;
-            cur = next;
-            idx++;
+            cur = nex;
+            nex = nex->next;
+            i++;
         }
 
-        if(firstCritical == -1 || firstCritical == lastCritical){
-            return {-1, -1};
+        int n = dis.size();
+
+        if(n<2) return {-1, -1};
+
+        int maxi = dis[n-1] - dis[0];
+
+        int mini  = dis[1] - dis[0];
+
+        for(int i = 1; i<n-1; i++)
+        {
+            mini  = min(mini , dis[i+1] - dis[i]);
         }
 
-        int maxDist = lastCritical - firstCritical;
-
-        return {minDist, maxDist};
+        vector<int>z = {mini , maxi};
+        return z;        
     }
 };
